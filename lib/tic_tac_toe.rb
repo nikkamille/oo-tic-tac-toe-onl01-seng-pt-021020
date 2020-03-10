ed4296fd526253a106df3f4bb2018d465a42dbf5
Kamille Longalong <k.pagibigan@gmail.com>
2020-03-10 23:07:55 +0000

    Automatically backed up by Learn

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,69 @@
+require 'pry'
+
+class TicTacToe
+  
+  def initialize
+    @board = Array.new(9, " ")
+  end
+  
+  WIN_COMBINATIONS = [
+      [0,1,2],
+      [3,4,5],
+      [6,7,8],
+      [0,3,6],
+      [1,4,7],
+      [2,5,8],
+      [0,4,8],
+      [6,4,2]
+      ]
+      
+  def display_board
+    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+    puts "-----------"
+    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+    puts "-----------"
+    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def input_to_index(input)
+    input.to_i - 1
+  end
+  
+  def move(index, token)
+    @board[index] = token 
+  end
+  
+  def position_taken?(index)
+    @board[index] == " " ? false : true
+  end
+  
+  def valid_move?(index)
+    @board[index] == " " ? true : false
+  end
+  
+  def turn_count
+    @board.count{|index| index != " "}
+  end
+  
+  def current_player
+    self.turn_count.even? ? "X" : "O"
+  end
+  
+  def turn
+    input = gets.chomp
+    
+    # puts input
+    
+    index = self.input_to_index(input)
+    token = self.current_player
+    if self.valid_move?(index)
+      self.move(index, token)
+      self.display_board
+    else
+      self.turn
+    end
+  end
+  
+  
+  
+end
